b1422967f911f83fd04083ac1129ae535a169f2e
Lucas <lucas_mamani_12@hotmail.com>
2026-02-10 09:33:30 -0300

    router para agregar producto nuevos listo

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router(); //Se lo usa como app en el principal pero despu
 const mongoose = require("mongoose"); //se tiene que tener en cuenta que tendras mongoose conectado para que ande
 const MiModelo = require("../models/TestUsuario");
 // Importar el modelo
+const auth = require("../models/authToken.js");
 
 router.get("/product", async (req, res) => {
   try {
@@ -25,6 +26,43 @@ router.get("/product", async (req, res) => {
   }
 });
 
+router.post("/newproduct", auth, async (req, res) => {
+  const { nombre, costo, imagen, descripcion } = req.body;
+  try {
+    // req.user viene del token
+    const email = req.user.email;
+
+    // Buscamos al usuario en la base de datos
+    const doc = await MiModelo.findOne();
+    const user = doc.user.find((u) => u.email === email);
+    if (!user)
+      return res.status(404).json({
+        message:
+          "Usuario no encontrado, no tiene permitido publicar nuevo producto",
+      });
+
+    const nuevoProducto = {
+      _id: doc.product.length + 1,
+      nombre,
+      costo,
+      descripcion,
+      imagen,
+    };
+
+    // Agregar el producto al arreglo 'product' del documento de usuario
+    doc.product.push(nuevoProducto);
+
+    // Guardar el documento actualizado
+    await doc.save();
+
+    res.status(200).json({
+      message: "Producto agregado exitosamente",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error al obtener agregar producto" });
+  }
+});
 module.exports = router;
 
 //Anda route
